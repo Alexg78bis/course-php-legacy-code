fddc6a6008bab4b669fa883ad7a23418da277140
alexandre <alexanregiannetto@thenetgroup.net>
2019-05-11 14:08:27 +0200

    - bad setter

--- www/Model/User.php
+++ www/Model/User.php
@@ -27,7 +27,7 @@ class User implements UserInterface
     // TODO: move to repository
     public function setPwd($pwd)
     {
-        $this->pwd = password_hash($pwd, PASSWORD_DEFAULT);
+        $this->pwd = $pwd;
     }
 
     public function setRole($role)
