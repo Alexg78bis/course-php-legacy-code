7df28d01eb546ede58de06bfbc5a8cbf59eeaf2e
alexandre <alexanregiannetto@thenetgroup.net>
2019-05-11 18:06:44 +0200

    - OUPS 🤭 this PageController was necessary

---
+++ www/Controller/PagesController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Controller;
+
+use Core\View;
+
+class PagesController
+{
+    public function defaultAction(): void
+    {
+        $view = new View('homepage', 'back');
+        $view->assign('pseudo', 'prof');
+    }
+}
